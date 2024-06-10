5750bd138bb839f8f11075754702fff90d25f323
JOBIN K ABRAHAM <110444926+jobinkabraham21@users.noreply.github.com>
2024-06-10 13:31:52 +0530

    Add files via upload

--- mytodo/core/views.py
+++ mytodo/core/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.forms import TodoForm
+from core.models import Todo
 
 def home(request):
-    return render(request,'home.html')
+    form = TodoForm()
+    todos = Todo.objects.all()
+    print(form)
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+     
+    return render(request,'home.html',{'form':form, 'todos': todos})
